6bafb5ca03c2269a5af34cc2c888c68a7a6ea77e
aurora <56488329+auurora@users.noreply.github.com>
2020-12-22 21:54:36 +0000

    Update shuffle.js

--- shuffle.js
+++ shuffle.js
@@ -1,14 +1,18 @@
-let buttons = document.getElementsByTagName("button");
-let shuffle_button;
+{
+    let buttons = document.getElementsByTagName("button");
+    let shuffle_button;
 
-const Time = 30000; // 30 Seconds
+    const Time = 30000; // 30 Seconds
 
-for (let i in buttons)
-    if (buttons[i].title == "Disable shuffle")
-        shuffle_button = buttons[i];
-if (shuffle_button == null) 
-    console.error("could not find shuffle button :(");
-else {
-    setInterval(() => shuffle_button.click(), Time);
-    setInterval(() => shuffle_button.click(), Time + 1000);
+    for (let i in buttons)
+        if (buttons[i].title == "Disable shuffle")
+            shuffle_button = buttons[i];
+    if (shuffle_button == null) 
+        console.error("could not find shuffle button :(");
+    else {
+        setInterval(() => {
+            shuffle_button.click();
+            setTimeout(()=>shuffle_button.click(), 1000);
+        }, Time);
+    }
 }
